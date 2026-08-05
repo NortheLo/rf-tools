34a433dc6ddf349a38760c3ef61782b748ab0e80
NortheLo <louis@northe-online.de>
2026-08-05 13:47:33 +0200

    added source for ref values

--- src/telcom_equations/friis_transmission_equation.rs
+++ src/telcom_equations/friis_transmission_equation.rs
@@ -1,36 +1,42 @@
 use num_traits::Float;
 use crate::conversion::lin_to_db_amp;
-pub fn friis_trans_equation_lin<T: Float>(
+pub fn friis_equation_lin<T: Float>(
     p_tx: T,
     g_tx: T,
     g_rx: T,
     lambda: T,
     distance: T,
 ) -> T {
-    let four = T::from(4.0).unwrap();
-    let pi = T::from(std::f64::consts::PI).unwrap();
+    let four_pi = T::from(4.0 * std::f64::consts::PI).unwrap();
 
-    let factor = lambda / (four * pi * distance);
+    let factor = lambda / (four_pi * distance);
     p_tx * g_tx * g_rx * factor.powi(2)
 }
 
-pub fn friis_trans_equation_log<T: Float>(
+pub fn friis_equation_log<T: Float>(
     p_tx: T,
     g_tx: T,
     g_rx: T,
     lambda: T,
     distance: T,
 ) -> T {
-    let four = T::from(4.0).unwrap();
-    let pi = T::from(std::f64::consts::PI).unwrap();
+    let four_pi = T::from(4.0 * std::f64::consts::PI).unwrap();
 
-    p_tx + g_tx + g_rx + lin_to_db_amp(lambda / (four * pi * distance))
+    p_tx + g_tx + g_rx + lin_to_db_amp(lambda / (four_pi * distance))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::conversion::*;
+
+    // Reference Values can be generated here: https://electrotopic.com/de/friis-ubertragungsrechner/#gsc.tab=0
+    // Settings are:
+    // Pt = 10 Watts
+    // Gt = 3
+    // Gr = 3
+    // lambda = 0.5
+    // R = 2
 
     const EPS: f64 = 1e-12;
 
@@ -51,7 +57,7 @@ mod tests {
         let distance = 2.0;
 
         let result =
-            friis_trans_equation_lin(p_tx, g_tx, g_rx, lambda, distance);
+            friis_equation_lin(p_tx, g_tx, g_rx, lambda, distance);
 
         let expected = 0.015831434944115281;
         approx_eq(result, expected);
@@ -65,7 +71,7 @@ mod tests {
         let lambda = 0.5;
         let distance = 2.0;
 
-        let result = friis_trans_equation_log(
+        let result = friis_equation_log(
             w_to_dbm(p_tx),
             lin_to_db_pwr(g_tx),
             lin_to_db_pwr(g_rx),
