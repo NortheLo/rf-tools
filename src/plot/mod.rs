340d0383e8fe65e6be93341558aabe234ebc3861
NortheLo <louis@northe-online.de>
2026-09-11 16:42:30 +0200

    added optional plot of range_doppler matrices

---
+++ src/plot/mod.rs
@@ -0,0 +1,4 @@
+pub mod range_doppler;
+
+#[cfg(feature = "plot")]
+pub mod plot;
---
+++ src/plot/range_doppler.rs
@@ -0,0 +1,21 @@
+use ndarray::ArrayView2;
+use ruviz::prelude::*;
+
+pub fn range_doppler(
+    rdm: &ArrayView2<f32>,
+    filename: &str,
+) -> PlotResult<()> {
+
+    Plot::new()
+        .dpi(1024)
+        .heatmap(rdm)
+        .title("Range-Doppler Map")
+        .xlabel("Doppler Bin [Hz]")
+        .ylabel("Range Bin [ ]")
+        .colorbar_label("Power [dB]")
+        .cmap(ColorMap::viridis())
+        .theme(Theme::publication())
+        .typst(true)
+        .save(filename)?;
+    Ok(())
+}
